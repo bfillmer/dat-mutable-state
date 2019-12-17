6a13f44f2c649103b20c9aaefa079f4f4cb8b567
Bryan Fillmer <bryan.fillmer@gmail.com>
2019-12-17 11:01:17 -0700

    First pass at a fake api call and creating a ref object to hold overall data.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { useFormState } from 'react-use-form-state'
 
+// UTILITIES
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 // ITEM
 const initialItemState = {
   qty: ''
@@ -21,29 +26,71 @@ function Item({id, title}) {
 }
 
 // PROVIDER
-const items = [
-  { id: 101, title: 'Calc I' },
-  { id: 201, title: 'Calc II' },
-  { id: 301, title: 'Calc III' }
-]
+
+// Simulate API call for data.
+async function getItems(setItems) {
+  await timeout(500)
+  const items = [
+    { id: 101, title: 'Calc I' },
+    { id: 201, title: 'Calc II' },
+    { id: 301, title: 'Calc III' }
+  ]
+
+  setItems(items)
+  return items
+}
 
 const ItemContext = React.createContext([])
 
 function Provider({children}) {
+  console.log(`Provider Render`)
+  const [items, setItems] = React.useState([])
+  const itemsFormStateRef = React.useRef({})
+
+  function showRef () {
+    console.log('Current Form State', itemsFormStateRef.current)
+  }
+
+  // Setup our formStateRef based on new item data.
+  const resetFormStateRef = React.useCallback((newItems) => {
+    itemsFormStateRef.current = {}
+    if (newItems.length > 0) {
+      newItems.forEach(i => {
+        itemsFormStateRef.current = {
+          ...itemsFormStateRef.current,
+          [i.id]: initialItemState
+        }
+      })
+    }
+  }, [])
+
+  // Would need to rerun processItems when pagination changes the items.
+  React.useEffect(() => {
+    async function processItems() {
+      const items = await getItems(setItems)
+      resetFormStateRef(items)
+    }
+
+    processItems()
+  }, [resetFormStateRef])
+  
   return (
-    <ItemContext.Provider value={items}>
-      {children}
+    <ItemContext.Provider value={{items, showRef}}>
+      {(items.length > 0) && children}
     </ItemContext.Provider>
   )
 }
 
 function List() {
-  const items = React.useContext(ItemContext)
-  
+  const {items, showRef} = React.useContext(ItemContext)
+
   return (
-    <ul>
-      {items.map(i => <Item {...i} key={i.id} />)}
-    </ul>
+    <>
+      <button onClick={showRef}>Show Ref State</button>
+      <ul>
+        {items.map(i => <Item {...i} key={i.id} />)}
+      </ul>
+    </>
   )
 }
 
